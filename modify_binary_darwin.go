7ea0e72a9bf834a3fe3701b829fe63b0feb98851
casperhe <casperhe@tencent.com>
2020-06-04 11:22:55 +0800

    fix modifyBinary

--- modify_binary_darwin.go
+++ modify_binary_darwin.go
@@ -8,11 +8,11 @@ import (
 func modifyBinary(target uintptr, bytes []byte) {
 	kkk := 88888
 	fmt.Println(kkk)
-	fmt.Println("KKKKKKKKKKKKKKKK")
+	fmt.Println("XXXXXXX")
 	function := entryAddress(target, len(bytes))
 
 	page := entryAddress(pageStart(target), syscall.Getpagesize())
-	err := syscall.Mprotect(page, syscall.PROT_READ|syscall.PROT_WRITE)
+	err := syscall.Mprotect(page, syscall.PROT_WRITE)
 	if err != nil {
 		panic(err)
 	}
