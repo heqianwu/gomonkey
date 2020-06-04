e263f2eedaffe7ca7439507c5d39e4070c1bc3ad
casperhe <casperhe@tencent.com>
2020-06-04 11:06:14 +0800

    修改modifyBinary

--- modify_binary_darwin.go
+++ modify_binary_darwin.go
@@ -1,19 +1,25 @@
 package gomonkey
 
-import "syscall"
+import (
+	"fmt"
+	"syscall"
+)
 
 func modifyBinary(target uintptr, bytes []byte) {
-    function := entryAddress(target, len(bytes))
-    
-    page := entryAddress(pageStart(target), syscall.Getpagesize())
-    err := syscall.Mprotect(page, syscall.PROT_READ|syscall.PROT_WRITE|syscall.PROT_EXEC)
-    if err != nil {
-        panic(err)
-    }
-    copy(function, bytes)
-    
-    err = syscall.Mprotect(page, syscall.PROT_READ|syscall.PROT_EXEC)
-    if err != nil {
-        panic(err)
-    }
+	kkk := 88888
+	fmt.Println(kkk)
+	fmt.Println("KKKKKKKKKKKKKKKK")
+	function := entryAddress(target, len(bytes))
+
+	page := entryAddress(pageStart(target), syscall.Getpagesize())
+	err := syscall.Mprotect(page, syscall.PROT_READ|syscall.PROT_WRITE)
+	if err != nil {
+		panic(err)
+	}
+	copy(function, bytes)
+
+	err = syscall.Mprotect(page, syscall.PROT_READ|syscall.PROT_EXEC)
+	if err != nil {
+		panic(err)
+	}
 }
